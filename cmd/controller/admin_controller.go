699683208d26504f46304d811e10a4604cd38d07
danielamartinscosta <daniela.mc2011@hotmail.com>
2025-04-13 03:06:35 -0300

    feat: add entities controllers

---
+++ cmd/controller/admin_controller.go
@@ -0,0 +1,70 @@
+package controller
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/DRP14-S7-G002/api-ventuFrio/Internal/models"
+	"github.com/DRP14-S7-G002/api-ventuFrio/internal/db"
+	"github.com/gin-gonic/gin"
+)
+
+func GetAllAdmins(c *gin.Context) {
+	var admins []models.Admin
+	db.DB.Find(&admins)
+	c.JSON(http.StatusOK, admins)
+}
+
+func GetAdminByID(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	var admin models.Admin
+
+	if err := db.DB.First(&admin, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Admin não encontrado"})
+		return
+	}
+	c.JSON(http.StatusOK, admin)
+}
+
+func CreateAdmin(c *gin.Context) {
+	var admin models.Admin
+
+	if err := c.ShouldBindJSON(&admin); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	db.DB.Create(&admin)
+	c.JSON(http.StatusCreated, admin)
+}
+
+func UpdateAdmin(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	var admin models.Admin
+
+	if err := db.DB.First(&admin, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Admin não encontrado"})
+		return
+	}
+
+	if err := c.ShouldBindJSON(&admin); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	db.DB.Save(&admin)
+	c.JSON(http.StatusOK, admin)
+}
+
+func DeleteAdmin(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	var admin models.Admin
+
+	if err := db.DB.First(&admin, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Admin não encontrado"})
+		return
+	}
+
+	db.DB.Delete(&admin)
+	c.Status(http.StatusNoContent)
+}
---
+++ cmd/controller/agendamento_controller.go
@@ -0,0 +1,71 @@
+package controller
+
+import (
+	"github.com/DRP14-S7-G002/api-ventuFrio/internal/db"
+
+	"net/http"
+	"strconv"
+
+	"github.com/DRP14-S7-G002/api-ventuFrio/Internal/models"
+	"github.com/gin-gonic/gin"
+)
+
+func GetAllAgendamentos(c *gin.Context) {
+	var agendamentos []models.Agendamento
+	db.DB.Find(&agendamentos)
+	c.JSON(http.StatusOK, agendamentos)
+}
+
+func GetAgendamentoByID(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	var agendamento models.Agendamento
+
+	if err := db.DB.First(&agendamento, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Agendamento não encontrado"})
+		return
+	}
+	c.JSON(http.StatusOK, agendamento)
+}
+
+func CreateAgendamento(c *gin.Context) {
+	var agendamento models.Agendamento
+
+	if err := c.ShouldBindJSON(&agendamento); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	db.DB.Create(&agendamento)
+	c.JSON(http.StatusCreated, agendamento)
+}
+
+func UpdateAgendamento(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	var agendamento models.Agendamento
+
+	if err := db.DB.First(&agendamento, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Agendamento não encontrado"})
+		return
+	}
+
+	if err := c.ShouldBindJSON(&agendamento); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	db.DB.Save(&agendamento)
+	c.JSON(http.StatusOK, agendamento)
+}
+
+func DeleteAgendamento(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	var agendamento models.Agendamento
+
+	if err := db.DB.First(&agendamento, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Agendamento não encontrado"})
+		return
+	}
+
+	db.DB.Delete(&agendamento)
+	c.Status(http.StatusNoContent)
+}
