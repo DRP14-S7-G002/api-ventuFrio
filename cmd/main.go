16b7a392e6007b86fe0935d4af1d10bc29195f24
danielamartinscosta <daniela.mc2011@hotmail.com>
2025-04-13 03:10:29 -0300

    feat: update file main.go

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/DRP14-S7-G002/api-ventuFrio/Internal/db"
+	"github.com/DRP14-S7-G002/api-ventuFrio/cmd/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	gin.SetMode(gin.ReleaseMode)
+
+	err := db.InitDB()
+	if err != nil {
+		log.Fatalf("Erro ao conectar no banco de dados: %v", err)
+		os.Exit(1)
+	}
+
+	r := gin.Default()
+
+	routes.RegisterRoutes(r)
+
+	err = r.Run(":8080")
+	if err != nil {
+		log.Fatalf("Erro ao iniciar o servidor: %v", err)
+	}
+}
