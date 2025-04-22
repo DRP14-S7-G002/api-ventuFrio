746460d159c6a45c03ae94b4854f239a21d76eb9
Daniela Martins <84486574+danielamartinscosta@users.noreply.github.com>
2025-04-22 00:58:42 -0300

    Develop (#5)
    * Feature/project modularization (#1)
    
    * feat: (main) start project
    
    * feat: project-modularization
    
    * feat: add entities models
    
    * feat: add repository layer
    
    * feat: add service layer
    
    * feat: add standardized response handler
    
    * feat: add entities controllers
    
    * feat: add database scritp and configurations
    
    * feat: add docker and configs
    
    * feat: add file gitignore
    
    * feat: add file go.work
    
    * feat: update files go.mod and go.sum
    
    * feat: update file main.go
    
    * feat: add readme
    
    * refactor: adjust imports and dockers config
    
    * refactor: adjust imports
    
    * Feature/project modularization (#2)
    
    * feat: project-modularization
    
    * feat: add entities models
    
    * feat: add repository layer
    
    * feat: add service layer
    
    * feat: add standardized response handler
    
    * feat: add entities controllers
    
    * feat: add database scritp and configurations
    
    * feat: add docker and configs
    
    * feat: add file gitignore
    
    * feat: add file go.work
    
    * feat: update files go.mod and go.sum
    
    * feat: update file main.go
    
    * feat: add readme
    
    * refactor: adjust imports and dockers config
    
    * refactor: adjust imports
    
    * refactor: update database
    
    * refactor: update docker
    
    * feat: add swagger
    
    * refactor: updating files according to database remodeling
    
    * fix: remove unnecessary files
    
    * fix: resolve database connection issue by loading .env
    
    * Feature/update readme (#4)
    
    * feat: correct file name e update readme file
    
    * feat: rename file internal

---
+++ internal_temp/db/database.go
@@ -0,0 +1,69 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/DRP14-S7-G002/api-ventuFrio/internal/models"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() error {
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("Arquivo .env não encontrado ou não pôde ser carregado")
+	}
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		user, password, host, port, dbName)
+
+	var db *gorm.DB
+	var err error
+
+	maxRetries := 10
+	for i := 0; i < maxRetries; i++ {
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			break
+		}
+		log.Printf("Tentando conectar ao banco de dados... (%d/%d)", i+1, maxRetries)
+		time.Sleep(3 * time.Second)
+	}
+
+	if err != nil {
+		return fmt.Errorf("falha ao conectar com o banco: %w", err)
+	}
+
+	DB = db
+
+	err = DB.AutoMigrate(
+		&models.Cliente{},
+		&models.Agendamento{},
+		&models.Orcamento{},
+		&models.OrdemDeServico{},
+		&models.Material{},
+	)
+	if err != nil {
+		log.Printf("Erro ao realizar AutoMigrate: %v", err)
+		return err
+	}
+
+	log.Println("Banco conectado e migrado com sucesso.")
+	return nil
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
---
+++ internal_temp/repository/material_repository.go
@@ -0,0 +1,46 @@
+package repository
+
+import (
+	"github.com/DRP14-S7-G002/api-ventuFrio/internal/models"
+	"gorm.io/gorm"
+)
+
+type MaterialRepository interface {
+	GetAll() ([]models.Material, error)
+	GetByID(id int) (models.Material, error)
+	Create(material models.Material) error
+	Update(id int, material models.Material) error
+	Delete(id int) error
+}
+
+type materialRepo struct {
+	db *gorm.DB
+}
+
+func NewMaterialRepository(db *gorm.DB) MaterialRepository {
+	return &materialRepo{db}
+}
+
+func (r *materialRepo) GetAll() ([]models.Material, error) {
+	var materiais []models.Material
+	err := r.db.Preload("WorkOrdem").Find(&materiais).Error
+	return materiais, err
+}
+
+func (r *materialRepo) GetByID(id int) (models.Material, error) {
+	var material models.Material
+	err := r.db.First(&material, id).Error
+	return material, err
+}
+
+func (r *materialRepo) Create(material models.Material) error {
+	return r.db.Create(&material).Error
+}
+
+func (r *materialRepo) Update(id int, updated models.Material) error {
+	return r.db.Model(&models.Material{}).Where("id = ?", id).Updates(updated).Error
+}
+
+func (r *materialRepo) Delete(id int) error {
+	return r.db.Delete(&models.Material{}, id).Error
+}
